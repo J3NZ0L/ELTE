2f0151a6801560e9d95561565159b05ee0fe8da0
J3NZ0L <jeneyzoltan2@gmail.com>
2024-01-06 10:47:12 +0100

    beadando javitott, 1. fazis sablonba, gyakorlas elkezdve

---
+++ I. szemeszter/Progalap/Gyakorlas/Progevfzhgyak/01.06/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public struct Bor
+        {
+            public int mennyiseg;
+            public int ar;
+        }
+        static void Main(string[] args)
+        {
+            //Beolvasás
+            string sor = Console.ReadLine();
+            int evekszama = int.Parse(sor.Split(' ')[0]);
+            int K = int.Parse(sor.Split(' ')[1]);
+            Bor[] bortomb = new Bor[evekszama];
+            for (int i = 0; i < evekszama; i++)
+            {
+                sor = Console.ReadLine();
+                bortomb[i].mennyiseg = int.Parse(sor.Split(' ')[0]);
+                bortomb[i].ar = int.Parse(sor.Split(' ')[1]);
+            }
+            List<int> evek2000 = new List<int>();
+            //1. feladat: Kiválogatás
+            for (int i = 0; i < evekszama; i++)
+            {
+                if (bortomb[i].mennyiseg > 2000)
+                {
+                    evek2000.Add(i);
+                }
+            }
+            evek2000.OrderBy(n => n);
+            Console.Write(evek2000.Count+" ");
+            if (evek2000.Count > 0)
+            {
+                for (int i = 0; i< evek2000.Count; i++)
+                {
+                    Console.Write(evek2000[i]);
+                }
+            }
+                //2. feladat: Eldöntés
+
+                //3. feladat: Kiválogatás
+
+            }
+    }
+}
